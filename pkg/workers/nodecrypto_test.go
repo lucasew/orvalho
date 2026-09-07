48af9684f0c15bd0d7c14ea898d8309b5f03e9e7
lucasew <lucas59356@gmail.com>
2026-09-07 11:32:21 -0300

    feat(script): add node:crypto Binding
    rand, randomUUID, sha256/sha1/md5 hashes.
    No official claim: tests need Buffer.

---
+++ pkg/workers/nodecrypto_test.go
@@ -0,0 +1,108 @@
+package workers
+
+import "testing"
+
+func runNodeCrypto(t *testing.T, src string) {
+	t.Helper()
+	iso := New("", Options{Imports: NodeScriptImports()})
+	if err := iso.ScriptMain(t.Context(), src, "t.js"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNodeCryptoIdentity(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		if (require("node:crypto") !== crypto) throw new Error("node:crypto identity");
+		if (typeof crypto.randomBytes !== "function") throw new Error("randomBytes");
+		if (typeof crypto.createHash !== "function") throw new Error("createHash");
+	`)
+}
+
+func TestNodeCryptoRandomBytes(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		var a = crypto.randomBytes(16);
+		var b = crypto.randomBytes(16);
+		if (a.length !== 16) throw new Error("len a " + a.length);
+		if (b.length !== 16) throw new Error("len b " + b.length);
+		var same = true;
+		for (var i = 0; i < 16; i++) {
+			if (a[i] !== b[i]) { same = false; break; }
+		}
+		if (same) throw new Error("two calls should differ");
+	`)
+}
+
+func TestNodeCryptoRandomFillSync(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		var u = new Uint8Array(8);
+		var before = Array.prototype.slice.call(u);
+		var ret = crypto.randomFillSync(u);
+		if (ret !== u) throw new Error("return identity");
+		var changed = false;
+		for (var i = 0; i < u.length; i++) {
+			if (u[i] !== before[i]) { changed = true; break; }
+		}
+		if (!changed) throw new Error("not mutated");
+	`)
+}
+
+func TestNodeCryptoRandomFill(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		var u = new Uint8Array(8);
+		crypto.randomFill(u, function (err, buf) {
+			if (err) throw err;
+			if (buf !== u) throw new Error("callback buf");
+			var n = 0;
+			for (var i = 0; i < u.length; i++) n |= u[i];
+			if (n === 0) throw new Error("still zero");
+		});
+	`)
+}
+
+func TestNodeCryptoRandomUUID(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		var u = crypto.randomUUID();
+		if (typeof u !== "string" || u.length !== 36) throw new Error("shape " + u);
+		if (u.charAt(14) !== "4") throw new Error("version " + u);
+		var c = u.charAt(19);
+		if ("89ab".indexOf(c) < 0) throw new Error("variant " + u);
+		if (crypto.randomUUID() === u) throw new Error("same uuid");
+	`)
+}
+
+func TestNodeCryptoCreateHash(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		var hex = crypto.createHash("sha256").update("abc").digest("hex");
+		if (hex !== "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad") {
+			throw new Error("sha256 abc " + hex);
+		}
+		var hashes = crypto.getHashes();
+		if (hashes.indexOf("sha256") < 0) throw new Error("getHashes sha256");
+		if (hashes.indexOf("sha1") < 0) throw new Error("getHashes sha1");
+		if (hashes.indexOf("md5") < 0) throw new Error("getHashes md5");
+	`)
+}
+
+func TestNodeCryptoErrors(t *testing.T) {
+	runNodeCrypto(t, `
+		var crypto = require("crypto");
+		function expectCode(fn, code) {
+			try {
+				fn();
+				throw new Error("should throw " + code);
+			} catch (e) {
+				if (e.code !== code) throw new Error("want " + code + " got " + e.code + " " + e);
+			}
+		}
+		expectCode(function () { crypto.randomBytes("nope"); }, "ERR_INVALID_ARG_TYPE");
+		expectCode(function () { crypto.randomBytes(-1); }, "ERR_OUT_OF_RANGE");
+		expectCode(function () { crypto.createHash("xyzzy"); }, "ERR_CRYPTO_UNKNOWN_DIGEST");
+		expectCode(function () { crypto.createHash(1); }, "ERR_INVALID_ARG_TYPE");
+	`)
+}
