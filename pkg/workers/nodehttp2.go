22d909a45e490deba11f1b9660a8ec225c25cfc8
lucasew <lucas59356@gmail.com>
2026-09-09 20:59:02 -0300

    fix(script): export http2.Http2ServerResponse
    Astro writeWebResponse does res instanceof
    Http2ServerResponse. A missing export is undefined
    and goja throws Value is not an object.

--- pkg/workers/nodehttp2.go
+++ pkg/workers/nodehttp2.go
@@ -28,6 +28,10 @@ func newNodeHTTP2(iso *Isolate) *goja.Object {
 	mustSet(obj, "createSecureServer", n.jsCreateServer)
 	mustSet(obj, "connect", n.jsDeniedOut)
 	mustSet(obj, "constants", iso.vm.NewObject())
+	// Astro writeWebResponse does `res instanceof Http2ServerResponse`.
+	// A missing export is undefined; goja then throws "Value is not an object".
+	mustSet(obj, "Http2ServerRequest", func(goja.ConstructorCall) *goja.Object { return nil })
+	mustSet(obj, "Http2ServerResponse", func(goja.ConstructorCall) *goja.Object { return nil })
 	mustSet(obj, "default", obj)
 	return obj
 }
